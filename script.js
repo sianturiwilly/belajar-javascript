d12226fd2ce38ee07b786b5f90db94e19c793174
sianturiwilly <prajudiwilliam10@gmail.com>
2022-11-06 16:22:37 +0700

    Commit ke-8

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-function cetakAngka(n) {
-    if(n === 0) {
-        return;
-    }
-    console.log(n);
-    cetakAngka(n-1);
-}
+// Manipulasi Array
 
-cetakAngka(10);
+// 1. Menambah isi Array
+// var arr = [];
+// arr[0] = "Willi";
+// arr[1] = 'Sianturi';
+// arr[2] = 'Nike';
+// arr[6] = 'Ardilla';
+
+// console.log(arr);
+
+// 2. Menghapus isi Array
+// var arr = ['Willi', 'Sianturi', 'Nike'];
+// arr[1] = undefined;
+// console.log(arr);
+
+// 3. Menampilkan isi array
+// var arr = ['Willi', 'Sianturi', 'Nike'];
+
+// for(var i = 0; i < arr.length; i++) {
+//     console.log('Mahasiswa ke-' + i + ' : ' + arr[i]);
+// }
+
+// Method pada array
+var arr = ['Willi', 'Sianturi', 'Nike'];
+// 1. Join
+// console.log(arr.join(' - '));
+
+// 2. Push & Pop
+// arr.push('Ardilla', 'Darwis');
+// arr.pop();
+// arr.pop();
+// console.log(arr.join(' - '));
+
+// 3. Unshift & Shift
+// arr.unshift('Darwis');
+arr.shift();
+console.log(arr.join(' - '));
